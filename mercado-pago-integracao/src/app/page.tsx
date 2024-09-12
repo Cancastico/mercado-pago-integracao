f8c96cccff0c02d1056c129315d2b84b0ee07473
Carlos Augusto <40805641+Cancastico@users.noreply.github.com>
2024-09-12 09:15:29 -0300

    fix

--- mercado-pago-integracao/src/app/page.tsx
+++ mercado-pago-integracao/src/app/page.tsx
@@ -9,23 +9,32 @@ dotenv.config();
 
 export default function Component() {
   const [selectedOption, setSelectedOption] = useState<Option>({ label: 'TESTE 1', description: '', value: 2, image: null })
-  const [step, setStep] = useState<'1' | '2'>('1')
+  const [step, setStep] = useState<number>(1)
   function nextStep() {
-    setStep('2')
+    setStep(prevStep => prevStep + 1);
   }
 
   return (
-    <section className="h-[100dvh] w-[100dvw] flex flex-col justify-center items-center">
-      {step == '1' && (
-        <PayMeCoffe
-          selectedOption={selectedOption}
-          setSelectedOption={setSelectedOption}
-          nextStep={nextStep}
-        ></PayMeCoffe>
+    <section className="h-[100dvh] w-[100dvw] flex flex-col">
+      {step == 1 && (
+        <div className=" w-full">
+          <PayMeCoffe
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            nextStep={nextStep}
+
+          ></PayMeCoffe>
+        </div>
       )}
-      {step == '2' && (
+      {step == 2 && (
         <div className="max-w-[40dvw] overflow-y-auto bg-white rounded-l scroll-smooth scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-w-1 scrollbar scrollbar-track-transparent scrollbar-thumb-red-600 hover:scrollbar-track-[#f1f5f9]">
-          <Checkout item={selectedOption} />
+          <Checkout item={selectedOption} nextStep={nextStep} />
+        </div>
+      )}
+
+      {step == 3 && (
+        <div className="max-w-[40dvw] overflow-y-auto bg-white rounded-l scroll-smooth scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-w-1 scrollbar scrollbar-track-transparent scrollbar-thumb-red-600 hover:scrollbar-track-[#f1f5f9]">
+          <p>FUNFOU</p>
         </div>
       )}
     </section>
--- mercado-pago-integracao/src/components/forms/checkOut/page.tsx
+++ mercado-pago-integracao/src/components/forms/checkOut/page.tsx
@@ -4,14 +4,16 @@ import PaymentCreate from '@/services/fetchs/payments/create';
 import { initMercadoPago, Payment } from '@mercadopago/sdk-react';
 type Props = {
   item: Option
+  nextStep: () => void,
 }
 
-const Checkout = ({ item }: Props) => {
-  
-  initMercadoPago(process.env.NEXT_PUBLIC_KEY!);
+const Checkout = ({ item, nextStep }: Props) => {
+
+  initMercadoPago(process.env.NEXT_PUBLIC_KEY!, { locale: 'pt-BR' });
 
   return (
     <Payment
+
       initialization={{
         amount: item.value,
         preferenceId: '<YOUR_PREFERENCE_ID>',
@@ -26,8 +28,14 @@ const Checkout = ({ item }: Props) => {
           payment_method_id: param.formData.payment_method_id,
           transaction_amount: param.formData.transaction_amount,
           token: param.formData.token,
+        })?.then((response)=>{
+          nextStep();
+        }).catch((error)=>{
+          console.error(error);
         })
       }}
+
+      onReady={nextStep}
     />
   );
 };
